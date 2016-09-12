c4ac9f5ab8f1058bb66639d315c6141a44aaf339
Usuario <Usuario@usuario-pc>
2016-09-12 00:58:30 -0300

    oi

--- src/tpcube/TpCube.java
+++ src/tpcube/TpCube.java
@@ -17,6 +17,8 @@ public class TpCube {
     /**
      * @param args the command line arguments
      */
+    
+      
     public static void main(String[] args) {
         // TODO code application logic here
         int dim;
@@ -29,7 +31,7 @@ public class TpCube {
         System.out.println("dim:"+dim);
       
         cube cb = new cube(dim);
-        int matrix[][] = new int[dim][dim];
+        char matrix[][] = new char[dim][dim];
         String mtx = "";
         
         for(int i  = 0; i < 6; i++){
@@ -38,7 +40,7 @@ public class TpCube {
             for(int j = 0; j < dim; j++){
                 for(int k = 0; k < dim; k++){
                     mtx = sc.next();
-                    matrix[j][k] = (int)mtx.charAt(0);
+                    matrix[j][k] = mtx.charAt(0);
                     System.out.print(mtx);
                     
                 }
@@ -68,10 +70,36 @@ public class TpCube {
                     System.out.println("str:"+str);
                     break;
             }
-            matrix = new int[dim][dim];
+            matrix = new char[dim][dim];
         }
-        cb.rotate(2);
+        
         cb.print();
+        cb.print();
+        
+        if(args.length == 7){
+            int epochs, size, elitismSize, k,maxIndividualSize;
+            double probCross, probMut;
+            epochs = Integer.parseInt(args[0]);
+            size = Integer.parseInt(args[1]);
+            probCross = Double.parseDouble(args[2]);
+            probMut = Double.parseDouble(args[3]);
+            k = Integer.parseInt(args[4]);
+            elitismSize = Integer.parseInt(args[5]);
+            maxIndividualSize = Integer.parseInt(args[6]);
+            
+            enviroment env;
+            System.out.println("epochs:" + epochs+ "\nsize:" + size+ "\nk:" + k+ 
+                    "\nelit:" + elitismSize+ "\nCross:" + probCross+ "\nMut:" + probMut); 
+            
+            env = new enviroment(epochs, size, maxIndividualSize, dim, probCross, probMut, k, cb, elitismSize);
+           
+            env.evolve();
+            
+        }
+        else{
+            System.out.println("Parametros errados");
+        }
+        
         
     }
     
