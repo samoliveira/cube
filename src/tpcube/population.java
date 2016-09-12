e692b3e34432ae95ff87064009aecc0597734606
samoliveira <>
2016-09-12 09:34:17 -0300

    popu

--- src/tpcube/population.java
+++ src/tpcube/population.java
@@ -8,6 +8,8 @@ package tpcube;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author samoliveira
@@ -33,38 +35,41 @@ public class population{
         */
         for(int i = 0; i < size; i++){
             group.add(new individual(in));
-            
-            group.get(i).initMoves(20+rnd.nextInt(maxIndividualSize), cube.getMaxMovesSize(dim));
+            group.get(i).initMoves(20+rnd.nextInt(maxIndividualSize), 17);
+            System.out.println(group.get(i).toString());
         }
     }
     
     public void select(int k, double probCross, double probMut, int maxMoveSize, cube in, int elitismSize){
         // o objetivo deste metodo é selecionar de 2 em dois inviduos até gerar N(size) novos;
-        individual parent1 = new individual(in);
-        individual parent2 = new individual(in);
+        individual parent1 ;
+        individual parent2 ;
         Random rnd = new Random();
         boolean Empty = true;
-        sons = new ArrayList<individual>();
+        sons = new ArrayList<>();
         
         // --- gera uma nova população
         // -------- ELISTISMO -------
         // copiamos o melhor individuo para a nova geração
-        
+        System.out.println("SELECT\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         int best = 0, maxFitness;
         
         for(int j = 0; j < elitismSize; j++){
             maxFitness = -1;
             for(int i = 0; i < group.size(); i++){
-                 if(maxFitness < group.get(j).getFitness()){
-                     best = j;
+                 if(maxFitness < group.get(i).getFitness()){
+                     best = i;
                  }
              }
+         //   System.out.println(group.get(best).toString());
+            sons.add(group.remove(best));
             
-            sons.add(group.get(best));
-            group.remove(best);
          }
-         if(sons.size()> 0)
+         if(sons.size()> 0){
             group.addAll(sons);    
+             
+         }
+         //print();
          
         // ---- OPERACOES GENETICAS ----
         //enche a lista de filhos ate termos N(size) filhos;
@@ -72,25 +77,35 @@ public class population{
             parent1 = tournament(k);
             parent2 = tournament(k);
             // cruzamento
-            if(rnd.nextDouble() <  probCross && Empty){
+            double nxt = rnd.nextDouble();
+            if(nxt <  probCross && Empty){
                 //faz o teste de limite populacional dentro do metodo crossover
                 // se há espaço para apenas 1 individuo seleciona o de maior fitness
-                crossover(in, parent1, parent2);  
+                crossover(in, parent1, parent2); 
+             //   System.out.println("Crossover "+probCross+ "next: "+nxt+" Empty:" + Empty);
+                if(sons.size() == size){
+                    Empty = false;
+                }
                 
             }
             // mutação
             if(rnd.nextDouble() < probMut && Empty){
-                parent1.mutate(maxMoveSize, in);
+                parent1.mutate(cube.getMaxMovesSize(in.getDim()));
                 sons.add(parent1);
                 if(sons.size() == size)
                     Empty = false;
+               // System.out.println("MUTOU parent1");
+               // System.out.println(""+parent1.toString());
             }
             if(rnd.nextDouble() < probMut && Empty){
-                parent2.mutate(maxMoveSize, in);
+                parent2.mutate(cube.getMaxMovesSize(in.getDim()));
                 sons.add(parent2);
                 if(sons.size() == size)
                     Empty = false;
+                //System.out.println("MUTOU parent2");
+              //  System.out.println(""+parent2.toString());
             }
+            
         }
         group.clear();
         group.addAll(sons);
@@ -120,7 +135,7 @@ public class population{
         // retorna uma lista com todas as fitness, com o fim de calcular as estatisticas da população
         double []data = new double[group.size()];
         for(int i = 0; i < group.size(); i++){
-            data[i] =(double)group.get(i).getFitness();
+            data[i] = group.get(i).getFitness();
         }
         return data;
     }
@@ -141,13 +156,32 @@ public class population{
     public boolean setGroupFitness(){
         boolean finish = false;
         for(int i = 0; i < group.size(); i++){
-            group.get(i).start();//threta hahaha
+            group.get(i).run();//threta hahaha
+            System.out.println(""+group.get(i).toString());
         }
+        for(int i = 0; i < group.size(); i++){
+            try {
+                group.get(i).join();//threta hahaha
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(population.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        group.get(0).in.print();
         if(getBest().getFitness() == individual.FINALFITNESS){
             finish = true;
         }
             
         return finish;
+    }
+    
+    public void print(){
+        System.out.println("--------------------printing group----------------");
+        for(individual ind:group){
+            System.out.println("-Fitness: " +ind.getFitness());
+            System.out.println(ind.toString());
+            
+        }
     }
     
     private individual tournament(int k){
@@ -160,7 +194,7 @@ public class population{
         ArrayList<Integer> IndList = new ArrayList<Integer>();
         int best = -1, MaxFitness = -1;
         
-        int []ListRand = new int[group.size()];
+        int []ListRand;
         ListRand = genListRandom(group.size());
        // for(int l:ListRand){
            // System.out.println("L: "+ l);
@@ -169,9 +203,10 @@ public class population{
             if(MaxFitness < group.get(ListRand[i]).getFitness()){
                 best = i;
                 MaxFitness = group.get(ListRand[i]).getFitness();
-                System.out.println("MAXFITNESS:::"+MaxFitness);
+             //   System.out.println("MAXFITNESS:::"+MaxFitness);
             }
         }  
+     //   System.out.println("MAXFITNESS:::"+MaxFitness);
         return group.get(best); 
     }
     
@@ -195,8 +230,8 @@ public class population{
         
         crosspoint1 = rnd.nextInt(ind1.getMoves().size());
         crosspoint2 = rnd.nextInt(ind2.getMoves().size());
-        System.out.println("ind1.size():" + ind1.getMoves().size());
-        System.out.println("ind2.size():" + ind2.getMoves().size());
+       // System.out.println("ind1.size():" + ind1.getMoves().size());
+       // System.out.println("ind2.size():" + ind2.getMoves().size());
         for(int i = 0; i < crosspoint1; i++){
             son1.getMoves().add(ind1.getMoves().get(i));
           //  System.out.println("ind1 i: "+i + " Crosspoint1:" + crosspoint1);
@@ -221,14 +256,19 @@ public class population{
         if(size == sons.size()-1){
             son1.setFitness();
             son2.setFitness();
-            if(son1.getFitness() > son2.getFitness())    
+            if(son1.getFitness() > son2.getFitness()){    
                 sons.add(son1);
-            else
+        //        System.out.println("ADDED SON CROSSOVER 1: " +son1.toString());
+            }else{
                 sons.add(son2);
+          //      System.out.println("ADDED SON CROSSOVER 2: " +son2.toString());
+            }
         }
         else{
             sons.add(son2);
+            //System.out.println("ADDED SON CROSSOVER 2: " +son2.toString());
             sons.add(son1);
+        //    System.out.println("ADDED SON CROSSOVER 1: " +son1.toString());
         }      
     }  
 }
